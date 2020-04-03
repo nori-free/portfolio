4efc63a7a2468d11287128afca131ede4a3ce9ab
nori <norinorimcz@gmail.com>
2020-04-03 20:26:58 +0900

    一旦push

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,6 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.js'],
+  stories: ['../stories/**/*.stories.js',
+  // '../stories/**/*.stories.ts', '../stories/**/*.stories.jsx', '../stories/**/*.stories.tsx',
+],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
 };
